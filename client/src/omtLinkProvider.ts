cf37efd8ea7175a1abb6acbc8fcc6aaec17b128e
Emiel Beeksma <emielb@users.noreply.github.com>
2022-08-08 15:54:10 +0200

    remove logging

--- client/src/omtLinkProvider.ts
+++ client/src/omtLinkProvider.ts
@@ -5,7 +5,7 @@ import { DocumentLinkRequest, CancellationToken, DocumentLinkParams, LanguageCli
  * Proxy for handling OMT document links with LSP
  */
 export default class OMTLinkProvider implements DocumentLinkProvider {
-    constructor(private client: LanguageClient) { console.log('client has started'); }
+    constructor(private client: LanguageClient) { }
 
     resolveDocumentLink(link: DocumentLink, token: CancellationToken) {
         // resolve in the language server
@@ -15,7 +15,6 @@ export default class OMTLinkProvider implements DocumentLinkProvider {
     provideDocumentLinks(document: TextDocument): Promise<DocumentLink[]> {
         const params: DocumentLinkParams = { textDocument: { uri: document.uri.toString() } };
         // send a request to the language server
-        console.log(`going to send ${JSON.stringify(params, undefined, 2)}  to server for document links`)
         return this.client.sendRequest(DocumentLinkRequest.method, params);
     }
 }
--- client/src/test/omtLinkProvider.test.ts
+++ client/src/test/omtLinkProvider.test.ts
@@ -12,12 +12,9 @@ describe('OMTLinkProvider', () => {
 
     before(async () => {
         sendRequestStub = stub(LanguageClient.prototype, 'sendRequest')
-            .callsFake((...args: any[]) => { // eslint-disable-line @typescript-eslint/no-explicit-any
-                console.log(`received lsp request with args: ${JSON.stringify(args)}`)
-                return Promise.resolve([
-                    new DocumentLink(toRange(1, 4, 1, 19))
-                ]);
-            });
+            .callsFake(() => Promise.resolve([
+                new DocumentLink(toRange(1, 4, 1, 19)),
+            ]));
 
         await activate(docUri);
         await new Promise(resolve => setTimeout(resolve, 150)); // language client needs some extra startup time apparently
@@ -33,9 +30,7 @@ describe('OMTLinkProvider', () => {
         // the making of the links should be tested for the server
         // and the rendering of the links is the responsibility of vscode
         const requestMethod = "textDocument/documentLink";
-        console.log('doing test')
         expect(sendRequestStub).to.be.calledWith(requestMethod, { textDocument: { uri: docUri.toString() } });
-        console.log('did test')
     });
 });
 
